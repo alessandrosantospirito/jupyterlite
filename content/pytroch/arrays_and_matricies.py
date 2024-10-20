f6bb9ecf78cf3f3941c0ea8e9007ee3b9b28c3c5
Your Name <you@example.com>
2024-10-20 09:52:08 +0000

    working on multi-lanes

--- content/pytroch/arrays_and_matricies.py
+++ content/pytroch/arrays_and_matricies.py
@@ -26,18 +26,37 @@ def maximum_value_by_grp(matrix, grp_idx=0, val_idx=1):
     matrix: torch.tensor([[ 0.,  2.], [ 0.,  3.], [ 1.,  0.], [ 1.,  3.], [ 1.,  4.], [ 2.,  23.], [ 3.,  4.], [ 0.,  7.], [ 1.,  0.], [ 1.,  3.], [ 1.,  7.], [ 0., 11.]])
     result: tensor([[ 0., 11.], [ 1.,  7.], [ 2., 23.], [ 3.,  4.]])
     """
-    num_groups = int(matrix[:, grp_idx].max().item()) + 1
-
-    result = torch.zeros(num_groups, 2)
     
-    result[:, grp_idx] = torch.arange(num_groups)
+    unique_grps = torch.unique(matrix[:, grp_idx])
+    count_unique_grps = unique_grps.shape[0]
+    result = torch.zeros(count_unique_grps, 2)
+    result[:, grp_idx] = unique_grps[:]
+    indices = (matrix[:, grp_idx][..., None] == unique_grps).nonzero(as_tuple=True)[1]
     
     result[:, val_idx] = torch.scatter_reduce(
-        input=torch.zeros(num_groups),
+        input=torch.zeros(count_unique_grps),
         dim=0,
-        index=matrix[:, grp_idx].long(),
+        index=indices.long(),
         src=matrix[:, val_idx],
         reduce='amax'
     )
-
+    
     return result
+
+def add_to_matrix_mapping_values(matrix, mapping, grp_idx = 0, val_idx = 1):
+    """
+    a = tensor([[ 0.,  1.], [ 0.,  5.], [ 0.,  3.], [ 0.,  5.], [ 0.,  6.], [ 1.,  7.], [ 1.,  6.], [ 1., 11.], [ 5.,  4.], [ 7., 10.]])
+    b = tensor([[ 0.,  0.], [ 1.,  7.], [ 5., 12.], [ 7.,  5.]])
+    c = tensor([0, 1, 0, 1, 1, 0, 1, 1, 0, 0])
+    d = tensor([[ 0.,  0.], [ 1.,  7.], [ 6., 19.], [13., 24.]])
+    e = tensor([[ 0.,  1.], [ 1., 12.], [ 0.,  3.], [ 1., 14.], [ 1., 13.], [ 0.,  4.], [ 1., 17.], [ 1., 18.], [ 0.,  5.], [ 0.,  6.]]))
+    """
+
+    a = matrix
+    b = mapping
+    c = (a[:, 0][..., None] == b[:, 0]).nonzero(as_tuple=True)[1]
+    d = torch.cumsum(b, dim=0)
+    e = a
+    e[:, 1] = e[:, 1] + d[c][:, 1]
+    
+    return e
